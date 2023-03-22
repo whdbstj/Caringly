ba69950da432fde13e4c7a10bb7059f180f9c70b
whdbstj <choevie080317@gmail.com>
2023-03-22 17:41:08 +0900

    d

--- app/src/main/java/cho/chloe/caringly/Splash.java
+++ app/src/main/java/cho/chloe/caringly/Splash.java
@@ -1,14 +1,26 @@
 package cho.chloe.caringly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    public void onCreate(Bundle splash) {
+        super.onCreate(splash);
         setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, MainPage.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
