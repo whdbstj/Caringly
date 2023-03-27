9b3589e6c059c5384227484a176dc4aa4d2095f9
whdbstj <choevie080317@gmail.com>
2023-03-27 16:51:40 +0900

    d

--- app/src/main/java/cho/chloe/caringly/MainPage.java
+++ app/src/main/java/cho/chloe/caringly/MainPage.java
@@ -1,6 +1,9 @@
 package cho.chloe.caringly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,45 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maingpage);
+        Button assistant = findViewById(R.id.assistant);
+        assistant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainPage.this, Assistant.class);
+                startActivity(it);
+            }
+        });
+        Button list = findViewById(R.id.support);
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainPage.this, List.class);
+                startActivity(it);
+            }
+        });
+        Button match = findViewById(R.id.match);
+        match.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainPage.this, Match.class);
+                startActivity(it);
+            }
+        });
+        Button aboutus = findViewById(R.id.aboutus);
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainPage.this, AboutUs.class);
+                startActivity(it);
+            }
+        });
+        Button mypage = findViewById(R.id.mypage);
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainPage.this, MyPage.class);
+                startActivity(it);
+            }
+        });
     }
 }
